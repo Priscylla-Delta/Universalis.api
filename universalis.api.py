b393d757790e08ce44cbdd209dbe4502170ad8a5
Priscylla <Priscylla.Delta@gmail.com>
2024-10-16 17:38:21 -0500

    Add files via upload

--- universalis.api.py
+++ universalis.api.py
@@ -3,13 +3,16 @@ import json
 
 
 
-marketable_Items_IDs = requests.get('https://universalis.app/api/v2/marketable', timeout=5)
-marketable_Items_IDs = json.loads(marketable_Items_IDs.text)
+marketable_Item_IDs = requests.get('https://universalis.app/api/v2/marketable', timeout=5)
+print("marketable_Item_IDs Status code: ",marketable_Item_IDs.status_code)
+marketable_Item_IDs = json.loads(marketable_Item_IDs.text)
 
 market_Data = requests.get('https://universalis.app/api/v2/North-america/2?listings=5&entries=0&statsWithin=0&fields=listings.pricePerUnit%2C+listings.worldName%2C+listings.worldID%2C+listings.quantity%2C+listings.total', timeout=5)
+print("market_Data Status code: ", market_Data.status_code)
 listings = market_Data.json()['listings']
 
 IDs_mapping = requests.get('https://raw.githubusercontent.com/ffxiv-teamcraft/ffxiv-teamcraft/master/libs/data/src/lib/json/items.json', timeout=5)
+print("IDs_mapping Status code : ", IDs_mapping.status_code)
 IDs_mapping = IDs_mapping.json()
 
 #print(IDs_mapping['1']['en'])
@@ -18,8 +21,15 @@ IDs_mapping = IDs_mapping.json()
 # for marketable_Item_ID in marketable_Items_IDs:
 #     print(IDs_mapping[marketable_Item_ID]['en'])
 
-for marketable_Item_ID in marketable_Items_IDs:
-    print(IDs_mapping[str(marketable_Item_ID)]['en'])
+marketable_Item_mapppings = {}
+for Item_ID in marketable_Item_IDs:
+    #print(IDs_mapping[str(marketable_Item_ID)]['en'])
+    #Item_ID = str(Item_ID)
+    marketable_Item_mapppings[Item_ID] = IDs_mapping[str(Item_ID)]['en']
+
+print(marketable_Item_mapppings)
+
+
 
 
 
