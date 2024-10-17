ac2ac8902096fe6a3a20352f10b0a64334d70f60
Priscylla <Priscylla.Delta@gmail.com>
2024-10-16 21:40:48 -0500

    Add files via upload
    Troubleshooting with Evie

--- universalis.api.py
+++ universalis.api.py
@@ -4,22 +4,25 @@ import sys
 
 def main():
 
-     # get all items ids sold on marketaboard
+    # get all items ids sold on marketaboard
     marketable_Item_IDs = get_marketable_Item_IDs()
     # get all items ids and their corrosponding in game names
     ID_Mappings = get_Id_Mappings()
 
-    # create a dictionary of all marketable items and their corosponding in game names
+    # Create a dictionary of all marketable items and their corosponding in game names
     marketable_Item_Mapppings = get_marketable_Item_Mapppings(marketable_Item_IDs, ID_Mappings)
 
     # Ask the user for a valid region and item name, referencing the created dictionary to validate item names
     validated_Input = get_validated_Input(marketable_Item_Mapppings)
 
-    # create a dictionary of all the listings for the given item and region
+    # Create a dictionary of all the listings for the given item and region
     listings = get_listings(validated_Input)
 
-    #use the listings and make a purchase order for the user
-    purchase_Order= get_purchase_Order(listings, validated_Input['quantity'])
+    # Use the listings and make a purchase order for the user
+    purchase_Order = get_purchase_Order(listings, validated_Input['quantity'])
+
+    # Render the order in some Gui
+    render_purchase_Order(purchase_Order, listings)
 
     return
 
@@ -126,52 +129,78 @@ def get_listings(validated_Input):
 
 def get_purchase_Order(listings, quantity):
     
+    temp_Listings = listings
+
     # Initialize a purchase order to store the list of listing Id's
-    purchase_order = []
+    purchase_Order = []
 
     while quantity > 0:
 
         # If there are no listings (should be checked elsewhere but alas)
-        if len(listings) == 0:
-            return purchase_order
+        if len(temp_Listings) == 0:
+            return purchase_Order
 
         # Iterate through a copy of each listing's id and grab the quantity being sold
-        for listing in list(listings):
-            listing_quantity = listings[listing]['quantity']
+        for listing in list(temp_Listings):
+            listing_quantity = temp_Listings[listing]['quantity']
             
             # If the listing quanitity is less or equal to the quantity being baught, subtract that amount
             # and add the listing id to the purchase order and remove the listingID from the listings Dict
             # such that the incrimentation backup doesnt reuse listings 
             if listing_quantity <= quantity:
                 quantity = quantity - listing_quantity
-                purchase_order.append(listing)
-                listings.pop(listing, None)
+                purchase_Order.append(listing)
+                #del temp_Listings[listing]
+                temp_Listings.pop(listing)
                 
 
             if quantity == 0:
                 # If youve bought exactly enough, stop the loop
                 # # Debugg for validating Purchase order structure, should be a list of Listing Ids
-                print(purchase_order)
-                return purchase_order
+                # print(purchase_Order)
+                return purchase_Order
             
         quantity += 1
 
 
+def render_purchase_Order(purchase_Order, listings):
+    #print(purchase_Order)
+    #print(listings)
 
 
 
-# def make_change(input, coins):
-#     used_Coins = []
-#     i = 0
-#     while input > 0:
-#         if coins[i] <= input:
-#             input = round(input - (coins[i]), ndigits=2)
-#             used_Coins.append(coins[i])
+    # print(purchase_Order[0])
+    # print(type(purchase_Order[0]))
+    
+    # for listing in listings:
+    #     print(type(listing))
+    #     print(listing)
+    #     sys.exit()
 
-#         else:
-#             i += 1
 
-#     return used_Coins
+    
+    first_purchase = purchase_Order[0]
+    print(first_purchase)
+
+    #print(listings)
+    print(listings[first_purchase])
+    for listing in listings:
+        print(listing)
+        return
+    
+
+    # for order in purchase_Order:
+    #     for listing in listings:
+    #         # print(listing)
+    #         # print(listings[listing]["listingID"])
+    #         print(listings[order])
+    #         sys.exit()
+
+
+
+
+
+    return
 
 
 main()
